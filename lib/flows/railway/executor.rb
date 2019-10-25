bfba78f45fbb0d58fbdd02cf29398317b662bd31
Roman Kolesnev <roman.kolesnev@vineti.com>
2019-10-25 17:50:22 +0400

    fix: possible bad behaviour in parallel code for Railway

--- lib/flows/railway/executor.rb
+++ lib/flows/railway/executor.rb
@@ -7,17 +7,25 @@ module Flows
       def initialize(flow:, class_name:)
         @flow = flow
         @railway_class_name = class_name
-        @context = {}
       end
 
       def call(**params)
-        last_result = @flow.call(ok(params), context: @context)
+        context = {}
+
+        last_result = @flow.call(ok(params), context: context)
+        patch_last_result(last_result, context)
+
+        last_result
+      end
+
+      private
+
+      # :reek:FeatureEnvy is allowed here because meta should be a Hash
+      def patch_last_result(last_result, context)
         last_meta = last_result.meta
 
         last_meta[:railway] = @railway_class_name
-        last_meta[:last_step] = @context[:last_step]
-
-        last_result
+        last_meta[:last_step] = context[:last_step]
       end
     end
   end
