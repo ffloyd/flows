4cdb0b7f41f3c3f6721d94527b11b04a5bfe7174
Roman Kolesnev <rvkolesnev@gmail.com>
2020-05-04 17:52:21 +0400

    refactor: Profiler Report structure

--- lib/flows/plugin/profiler/report.rb
+++ lib/flows/plugin/profiler/report.rb
@@ -1,3 +1,4 @@
+require_relative 'report/events'
 require_relative 'report/raw'
 
 module Flows
@@ -5,20 +6,40 @@ module Flows
     module Profiler
       # Base class for {Profiler} reports.
       #
-      # @!method add( event_type, klass, method_type, method_name, data )
-      #   @abstract
-      #   Add event to profile report.
-      #   @param event_type [:started, :finished] event type
-      #   @param klass [Class] class where called method is placed
-      #   @param method_type [:instance, :singleton] method type
-      #   @param method_name [Symbol] name of the called method
-      #   @param data [nil, Float] event data, time represented as
-      #     a Float microseconds value.
-      #
       # @!method to_s
       #   @abstract
       #   @return [String] human-readable representation.
       class Report
+        # @return [Array<Array>] raw profiler events
+        attr_reader :raw_data
+
+        def initialize
+          @raw_data = []
+        end
+
+        # Add event to profile report.
+        #
+        # @param event_type [:started, :finished] event type
+        # @param klass [Class] class where called method is placed
+        # @param method_type [:instance, :singleton] method type
+        # @param method_name [Symbol] name of the called method
+        # @param data [nil, Float] event data, time represented as
+        #   a Float microseconds value.
+        def add(*args)
+          raw_data << args
+        end
+
+        # @return [Array<Event>] array of events
+        def to_a
+          raw_data.map do |raw_event|
+            klass = case raw_event.first
+                    when :started then StartEvent
+                    when :finished then FinishEvent
+                    end
+
+            klass.new(*raw_event[1..-1])
+          end
+        end
       end
     end
   end
--- lib/flows/plugin/profiler/report/raw.rb
+++ lib/flows/plugin/profiler/report/raw.rb
@@ -4,43 +4,9 @@ module Flows
       class Report
         # Raw report. Preserves events as is.
         class Raw < Report
-          # @return [Array<Array>] raw profiler events
-          attr_reader :raw_data
-          alias to_a raw_data
-
-          def initialize
-            @raw_data = []
-          end
-
-          # @see Report#add
-          def add(*args)
-            raw_data << args
-          end
-
           # @see Report#to_s
           def to_s
-            raw_data.map { |event| render_event(*event) }.join("\n")
-          end
-
-          private
-
-          # :reek:ControlParameter
-          # :reek:LongParameterList
-          # :reek:TooManyStatements
-          def render_event(event_type, klass, method_type, method_name, data)
-            delimeter = case method_type
-                        when :instance then '#'
-                        when :singleton then '.'
-                        end
-
-            subject = "#{klass}#{delimeter}#{method_name}"
-
-            event_msg = case event_type
-                        when :started then 'started:'
-                        when :finished then "finished(#{data} microseconds):"
-                        end
-
-            "#{event_msg} #{subject}"
+            raw_data.map(&:to_s).join("\n")
           end
         end
       end
